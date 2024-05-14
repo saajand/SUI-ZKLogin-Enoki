b99d92d3cf8fb0f154bf4aa56f47c9652bfab2c5
saajand <saajanduwal2000@gmail.com>
2024-05-14 11:02:17 +0545

    Add Sponsored transaction without Enoki's sponsored transaction support

--- src/components/MintNFT.tsx
+++ src/components/MintNFT.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { useEnokiFlow } from "@mysten/enoki/react";
+import { useLogin } from "../context/UserContext";
 import { SuiClient } from "@mysten/sui.js/client";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
+import {
+  executeSponsoredTxn,
+} from "../utils/sponsorWalletUtils";
 
 const FULLNODE_URL = import.meta.env.VITE_APP_SUI_FULLNODE_URL as string;
 
 const MintNFT = () => {
   const flow = useEnokiFlow();
+  const { userDetails } = useLogin();
 
   const [loading, setLoading] = useState(false);
   const [txnDigest, setTxnDigest] = useState("");
@@ -50,12 +55,53 @@ const MintNFT = () => {
     }
   };
 
+  const handleNFTMintSponsored = async () => {
+    try {
+      setLoading(true);
+
+      // Mint Sample NFT
+      const txb = new TransactionBlock();
+      txb.moveCall({
+        target:
+          "0x5ea6aafe995ce6506f07335a40942024106a57f6311cb341239abf2c3ac7b82f::nft::mint",
+        arguments: [
+          txb.pure("Suiet NFT"),
+          txb.pure("Suiet Sample NFT"),
+          txb.pure(
+            "https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4"
+          ),
+        ],
+      });
+
+      const txnRes = await executeSponsoredTxn(
+        txb,
+        flow,
+        userDetails
+      );
+
+      console.log("txnRes", txnRes);
+
+      if (txnRes && txnRes?.digest) {
+        setTxnDigest(txnRes?.digest);
+        alert(`Sponsored NFT Minting Success. Digest: ${txnRes?.digest}`);
+      }
+    } catch (err) {
+      console.log("Error Minting NFT.", err);
+      alert("Error minting NFT. Check logs for details.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <h3>Mint NFT</h3>
 
       <div>
         <button onClick={handleNFTMint}>Mint NFT</button>
+        <button onClick={handleNFTMintSponsored}>
+          Mint NFT (Sponsored Txn)
+        </button>
         <div>{txnDigest && <div>Transaction Digest: {txnDigest}</div>}</div>
         {loading && <div>Loading...</div>}
       </div>
---
+++ src/utils/sponsorWalletUtils.ts
@@ -0,0 +1,124 @@
+import { SuiClient } from "@mysten/sui.js/client";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { fromB64 } from "@mysten/bcs";
+import { genAddressSeed, getZkLoginSignature } from "@mysten/zklogin";
+import { jwtDecode } from "jwt-decode";
+import { SerializedSignature } from "@mysten/sui.js/cryptography";
+import { EnokiFlow } from "@mysten/enoki";
+
+const FULLNODE_URL = import.meta.env.VITE_APP_SUI_FULLNODE_URL as string;
+const SPONSER_WALLET_PASSPHRASE = import.meta.env
+  .VITE_APP_SPONSER_WALLET_PASSPHRASE as string;
+
+export async function getSponsorPaymentObject(sponserAddress: string) {
+  const suiClient = new SuiClient({ url: FULLNODE_URL });
+
+  let payment: { objectId: string; version: string; digest: string }[] = [];
+  let retires = 10;
+  while (retires !== 0) {
+    const coins = await suiClient.getCoins({
+      owner: sponserAddress,
+      limit: 10,
+    });
+    if (coins.data.length > 0) {
+      payment = coins.data.map((coin) => ({
+        objectId: coin.coinObjectId,
+        version: coin.version,
+        digest: coin.digest,
+      }));
+      break;
+    }
+    await new Promise((resolve) => setTimeout(resolve, 200)); // Sleep for 200ms
+    retires -= 1;
+  }
+
+  return payment;
+}
+
+export async function executeSponsoredTxn(
+  txb: TransactionBlock,
+  enokiFlow: EnokiFlow,
+  userDetails: {
+    provider: string;
+    salt: string;
+    address: string;
+  }
+) {
+  const suiClient = new SuiClient({ url: FULLNODE_URL });
+
+  const sponsorKeypair = Ed25519Keypair.deriveKeypair(
+    SPONSER_WALLET_PASSPHRASE
+  );
+  const sponserAddress = sponsorKeypair.getPublicKey().toSuiAddress();
+  console.log("sponserAddress", sponserAddress);
+
+  const payment = await getSponsorPaymentObject(sponserAddress);
+  if (payment.length === 0) {
+    throw new Error("No payment object found in Sponsor wallet.");
+  }
+
+  await enokiFlow.getProof();
+  const session = await enokiFlow.getSession();
+
+  if (!session?.jwt) throw new Error("No JWT!");
+  if (!session?.proof) throw new Error("No ZKProof!");
+  if (!session?.ephemeralKeyPair) throw new Error("No ephemeralKeyPair!");
+  if (!userDetails?.address) throw new Error("No wallet address!");
+  if (!userDetails?.salt) throw new Error("No user salt!");
+
+  const kindBytes = await txb.build({
+    client: suiClient,
+    onlyTransactionKind: true,
+  });
+
+  // construct a sponsored transaction from the kind bytes
+  const sponsoredTxb = TransactionBlock.fromKind(kindBytes);
+  sponsoredTxb.setSender(userDetails.address);
+  sponsoredTxb.setGasOwner(sponserAddress);
+  sponsoredTxb.setGasPayment(payment);
+
+  const sponsoredTxnBuild = await sponsoredTxb.build({ client: suiClient });
+  const sponsoredSignedTxn = await sponsorKeypair.signTransactionBlock(
+    sponsoredTxnBuild
+  );
+
+  const ephemeralKeyPair = Ed25519Keypair.fromSecretKey(
+    fromB64(session?.ephemeralKeyPair)
+  );
+  const userSignedTxn = await ephemeralKeyPair.signTransactionBlock(
+    sponsoredTxnBuild
+  );
+
+  const decodedJwt = jwtDecode(session.jwt);
+
+  // Generate addressSeed using userSalt, sub, and aud (JWT Payload)
+  // as parameters for obtaining zkLoginSignature
+  const addressSeed: string = genAddressSeed(
+    BigInt(userDetails.salt),
+    "sub",
+    decodedJwt.sub as string,
+    decodedJwt.aud as string
+  ).toString();
+
+  const zkLoginSignature: SerializedSignature = getZkLoginSignature({
+    inputs: {
+      ...session.proof,
+      addressSeed,
+    },
+    maxEpoch: session.maxEpoch,
+    userSignature: userSignedTxn!.signature,
+  });
+
+  const txnRes = await suiClient.executeTransactionBlock({
+    transactionBlock: userSignedTxn!.bytes,
+    signature: [zkLoginSignature, sponsoredSignedTxn!.signature],
+    options: {
+      showEffects: true,
+      showEvents: true,
+      showObjectChanges: true,
+    },
+  });
+
+  return txnRes;
+}
